55a636a71dee23a7324c32f48f79b8fac471f83b
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2018-08-14 17:02:13 -0700

    Agregar archivos de proyecto.

---
+++ Tarea2/Enemigo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea2
+{
+    class Enemigo
+    {
+        public int Posicion_x { get; set; }
+        public int Posicion_y { get; set; }
+        private int _puntos;
+        public void Moverse()
+        { }
+        public void Disparar()
+        {
+
+        }
+        public void Explotar() { }
+        public int Puntos
+        {
+            get
+            {
+                return _puntos;
+            }
+            set
+            {
+                if (value >= 100)
+                {
+                    _puntos = value;
+                }
+                else
+                {
+                    Console.WriteLine("Introduce una puntuacion valida");
+                }
+            }
+        }
+    }
+}
---
+++ Tarea2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Enemigo enemigoUno = new Enemigo();
+            enemigoUno.Posicion_x = 50;
+            enemigoUno.Posicion_y = 20;
+            enemigoUno.Puntos = 150;
+            Console.ReadLine();
+        }
+    }
+}
